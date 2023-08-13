340e077f8405ce3aca6c1a7d537eea3667fdc55f
Nivedh <nivnair21@gmail.com>
2023-08-13 19:01:36 +0100

    updated module

--- CGF/src/Nam.py
+++
@@ -1,6 +0,0 @@
-from CGFCore import main
-from CGFCore.test import evaluate
-from CGFCore.games import PrisonersDilemmaGame
-# main.test_composed_game_sequential()
-# evaluate.run_tests()
-PrisonersDilemmaGame
